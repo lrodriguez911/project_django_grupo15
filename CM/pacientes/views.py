4053bb7b5e354d6acfac472d8bec5310378be53d
MaiineV <agustinmartinezbarrios2@hotmail.com>
2023-04-18 09:01:31 -0300

    Views configuradas

--- CM/pacientes/views.py
+++ CM/pacientes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def login_paciente():
+    documento = """<html><body><h1>Login Paciente</h1></body></html>"""
+    return HttpResponse(documento)
+
+def calendario_turnos_agendados():
+    documento = """<html><body><h1>Ver Turnos Agendados</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_turnos_disponibles():
+    documento = """<html><body><h1>Ver turnos disponibles</h1></body></html>"""
+    return HttpResponse(documento)
+
+def agendar_turno():
+    documento = """<html><body><h1>Plantilla para agendar turno y poner datos</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_perfil():
+    documento = """<html><body><h1>Ver mi ficha medica</h1></body></html>"""
+    return HttpResponse(documento)
