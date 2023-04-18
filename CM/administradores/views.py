9ad679d4f8295523ce1def11a0d9b382220917a1
MaiineV <agustinmartinezbarrios2@hotmail.com>
2023-04-18 09:06:01 -0300

    View Settings

--- CM/administradores/views.py
+++ CM/administradores/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def admin_login():
+    documento = """<html><body><h1>Admin Login</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_doctores():
+    documento = """<html><body><h1>Lista de Doctores</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_pacientes():
+    documento = """<html><body><h1>Lista de Pacientes</h1></body></html>"""
+    return HttpResponse(documento)
+
+def calendario_turnos():
+    documento = """<html><body><h1>Calendario Con turnos tomados y disponibles</h1></body></html>"""
+    return HttpResponse(documento)
--- CM/doctores/views.py
+++ CM/doctores/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def login_medico():
+    documento = """<html><body><h1>Login Medico</h1></body></html>"""
+    return HttpResponse(documento)
+
+def calendario_turnos():
+    documento = """<html><body><h1>Ver Turnos</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_consulta():
+    documento = """<html><body><h1>Ver resumen de consulta</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_plantilla_medica():
+    documento = """<html><body><h1>Ver la plantilla del paciente</h1></body></html>"""
+    return HttpResponse(documento)
+
+def ver_perfil():
+    documento = """<html><body><h1>Ver mi perfil</h1></body></html>"""
+    return HttpResponse(documento)
