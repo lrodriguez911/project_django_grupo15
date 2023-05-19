9f438c095781a0533f5b98fa0b286524483fc4fc
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-05-19 16:41:40 -0300

    agregados nuevos modelos

--- CM/doctores/models.py
+++ CM/doctores/models.py
@@ -1,14 +1,43 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
-class Consultas(models.Model):
+
+    
+class Doctor(models.Model):
+    license = models.IntegerField(primary_key=True)
+    dni = models.IntegerField()
     name = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    id = models.IntegerField(primary_key=True)
+    sex = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    email = models.EmailField()
+    phone_number = PhoneNumberField(blank=True)
+    especiality = models.ForeignKey('Especiality')
+    password = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True, blank=True)
-    vip = models.BooleanField(default=False)
-    # user = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-
+    updated = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.title + '- by ' + self.name
+
+class Especiality(models.Model):
+    id_especiality = models.AutoField(primary_key=True)
+    especiality = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+   
+    def __str__(self):
+        return self.especiality
+    
+class Calendar(models.Model):
+    id_calendar = models.AutoField(primary_key=True)
+    doctor = models.ForeignKey('Doctor')
+    day = models.DateField()
+    hour = models.TimeField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return 'Calendar of' + self.doctor
--- CM/pacientes/models.py
+++ CM/pacientes/models.py
@@ -1,15 +1,34 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
+from doctores.models import Doctor
 # Create your models here.
 
 class Paciente(models.Model):
-    dni = models.IntegerField()
+    dni = models.IntegerField(primary_key=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
+    sex = models.CharField(max_length=10)
+    birthday = models.DateField()
+    phone = PhoneNumberField(null=False, blank=False)
+    address = models.CharField(max_length=50)
+    email = models.EmailField()
+    vip = models.BooleanField(default=False)
+    password = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True, blank=True)
-    vip = models.BooleanField(default=False)
-    # user = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + '- by ' + self.name
+    
+class Consulta(models.Model):
+    id_consulta = models.AutoField(primary_key=True)
+    dni_paciente = models.ForeignKey(Paciente)
+    license_doctor = models.ForeignKey(Doctor)
+    date = models.DateField()
+    time_start = models.TimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    attended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title + '- by ' + self.name
